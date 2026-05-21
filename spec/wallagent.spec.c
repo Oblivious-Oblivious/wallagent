f6432ca3bd8c2906b32f8b1b2fe70fc384b9a719
atha <atha@fourth-wall.org>
2026-05-21 04:48:41 +0300

    [master] - removed unused files.

--- spec/wallagent.spec.c
+++ spec/wallagent.spec.c
@@ -1,6 +1,4 @@
 #include "../libs/cSpec/export/cSpec.h"
-#include "z/init.module.spec.h"
 
 int main(void) {
-  cspec_run_suite("all", { T_Z(); });
 }
--- src/z/init.c
+++
@@ -1,3 +0,0 @@
-#include "init.h"
-
-char *init(void) { return "Wall Agent"; }
