5b5e031c7c19efa97f4885f73ba901b7dabe9904
atha <atha@fourth-wall.org>
2026-06-20 19:15:47 +0300

    [master] - removed default files.

--- spec/wallagent.spec.c
+++ spec/wallagent.spec.c
@@ -1,6 +1,5 @@
 #include "../libs/cSpec/export/cSpec.h"
-#include "get-value/get-value.module.spec.h"
 
 int main(void) {
-  cspec_run_suite("all", {T_get_value();});
+  cspec_run_suite("all", {});
 }
